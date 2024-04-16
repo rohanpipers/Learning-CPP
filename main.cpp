15882a9692798fdb67764d443aa8d925b01353bb
Rohan Pipersenia <rohanpipers@gmail.com>
2024-04-17 01:29:49 +0900

    learning debugging with vscode

--- main.cpp
+++ main.cpp
@@ -2,6 +2,14 @@
 #include "Log.h"
 
 int main() {
+    int a = 8;
+    a++;
+    const char* string = "Hello";
+    
+    for(int i = 0; i < 5; i++){
+        const char c = string[i];
+        std::cout << c << std::endl;
+    }
     InitLog();
     Log("Hello World!");
     std::cin.get();
